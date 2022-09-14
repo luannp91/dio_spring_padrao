ca9d62be357e4ab8c99c647e529790af8d679424
Luan Pinto <luan.nelmo@hotmail.com>
2022-09-13 22:10:38 -0300

    Spring Boot DIO

---
+++ src/main/java/dio/web/api/service/implementacao/ClienteServiceImpl.java
@@ -0,0 +1,64 @@
+package dio.web.api.service.implementacao;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dio.web.api.model.Cliente;
+import dio.web.api.model.ClienteRepository;
+import dio.web.api.model.Endereco;
+import dio.web.api.model.EnderecoRepository;
+import dio.web.api.service.ClienteService;
+import dio.web.api.service.ViaCepService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ViaCepService viaCepService;
+	
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		return clienteRepository.findAll();
+	}
+	
+	@Override
+	public Cliente buscarPorId(Long id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
+	}
+	
+	@Override
+	public void inserir(Cliente cliente) {
+		salvarClienteComCep(cliente);
+	}
+	
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent()) {
+			salvarClienteComCep(cliente);
+		}
+	}
+	
+	@Override
+	public void deletar(Long id) {
+		clienteRepository.deleteById(id);
+	}
+	
+	private void salvarClienteComCep(Cliente cliente) {
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		clienteRepository.save(cliente);
+	}
+}
